60a055d2a1dab0c1b0a1b559002d1e07481c01dd
Raj Kapoor <rajkapoordev@gmail.com>
2022-07-04 10:59:00 +0530

    Init commit

---
+++ App.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type { Node } from 'react';
+
+import MainNavigation from './src/navigations';
+
+const App: () => Node = () => {
+  return <MainNavigation />;
+};
+
+export default App;
---
+++ src/navigations/index.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
+
+function HomeScreen() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+const MainNavigation = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default MainNavigation;
